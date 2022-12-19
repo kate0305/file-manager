b4fa5f171ba820c72a88f023d2e8d4dfd7532d57
Katsiaryna Voronich <katarinka403@gmail.com>
2022-12-20 01:11:55 +0300

    feat: implement validation of arguments

---
+++ src/validateArgs.js
@@ -0,0 +1,16 @@
+import { parseArgs } from './parseArgs.js';
+import { resolve } from 'node:path';
+import { cwd } from 'node:process';
+
+export const getPath = async (str) => {
+  try {
+    const { args } = parseArgs(str);
+    if (!args.length) throw new Error('Invalid input'); //отображается 2 раза сообщение при OS
+    const paths = args.map(arg => {
+      return resolve(cwd(), arg);
+    });
+    return paths;
+  } catch (error) {
+    console.log('getPath');
+  }
+};
